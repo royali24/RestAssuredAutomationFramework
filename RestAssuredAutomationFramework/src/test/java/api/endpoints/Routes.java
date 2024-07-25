55d4dbe3a80950d416fd28d4ac876a9d9f683355
royali24 <royali.roy24@gmail.com>
2024-07-24 21:26:54 -0400

    Commit First

---
+++ RestAssuredAutomationFramework/src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,37 @@
+package api.endpoints;
+/*This class is to maintain only URLs of every module and not any methods*/
+public class Routes {
+	/*Base URL: https://petstore.swagger.io/v2 */
+	//Post to Create User
+	//endpoint: user 
+	//url :https://petstore.swagger.io/v2/user
+	
+	//Get to get user 
+		//endpoint: user
+	    //path parameter: {username} 
+		//url :https://petstore.swagger.io/v2/user/{username}
+	
+	//Put to update user
+		//endpoint: user
+		//path parameter: {username} 
+		//url :https://petstore.swagger.io/v2/user/{username}	
+	
+	//Delete for deleting user
+	    //endpoint: user
+	    //path parameter: {username} 
+	    //url :https://petstore.swagger.io/v2/user/{username}	
+	
+	
+	public static String base_url ="https://petstore.swagger.io/v2";
+	
+	//user module urls	
+	public static String post_url = base_url+"/user";
+	public static String get_url = base_url+"/user/{username}";
+	public static String put_url = base_url+"/user/{username}";
+	public static String delete_url = base_url+"/user/{username}";
+	
+	// pet module urls
+	
+	// Store module urls
+}
+
---
+++ RestAssuredAutomationFramework/src/test/java/api/endpoints/UserEndPoints.java
@@ -0,0 +1,79 @@
+package api.endpoints;
+import com.aventstack.extentreports.gherkin.model.Given;
+
+import api.payload.UserPayload;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+public class UserEndPoints {
+
+	//Post - create user
+	//Payload :
+	/*
+	 *
+		[
+		  {
+		    "id": 0,
+		    "username": "string",
+		    "firstName": "string",
+		    "lastName": "string",
+		    "email": "string",
+		    "password": "string",
+		    "phone": "string",
+		    "userStatus": 0
+		  }
+		]
+	 */
+	
+	public static Response createUser(UserPayload payload)
+	{
+		Response response = given()
+		.accept(ContentType.JSON)
+		.contentType(ContentType.JSON)
+		.body(payload)
+		
+		.when()
+		.post(Routes.post_url);
+		
+		return response;
+	}
+	public static Response getUser(String userName)
+	{
+		Response response = given()
+		.accept(ContentType.JSON)
+		.pathParam("username", userName)
+		
+		.when()
+		.get(Routes.get_url);
+		
+		return response;
+	}
+	
+	public static Response updateUser(UserPayload payload, String userName)
+	{
+		Response response = given()
+		.accept(ContentType.JSON)
+		.contentType(ContentType.JSON)
+		.pathParam("username", userName)
+		.body(payload)
+		
+		.when()
+		.put(Routes.put_url);
+		
+		return response;
+	}
+	
+	public static Response deleteUser(String userName)
+	{
+		Response response = given()
+		.accept(ContentType.JSON)
+		.pathParam("username", userName)
+	
+		
+		.when()
+		.put(Routes.delete_url);
+		
+		return response;
+	}
+}
